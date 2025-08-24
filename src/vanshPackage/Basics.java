387614c610a575801405ecf8480d3795c59f3ce4
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-08-24 19:28:40 +0530

    basic code

---
+++ src/vanshPackage/Basics.java
@@ -0,0 +1,21 @@
+package vanshPackage;
+
+public class Basics {
+    public static void main(String[] args) {
+        if(true){
+            System.out.println("Hello world");
+        }
+        //while loop
+        int count = 1 ;
+        while(count !=5 ){
+            System.out.println(count);
+            count++;
+        }
+
+        //for loop
+        for (int c =1;c != 5; c++){
+            System.out.println(c);
+        }
+
+    }
+}
