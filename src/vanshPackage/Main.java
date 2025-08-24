1f22729e69b7e678a2398b6ea802a877d5ff58bb
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-08-24 18:55:51 +0530

    Dsa in java

---
+++ src/vanshPackage/Main.java
@@ -0,0 +1,14 @@
+package vanshPackage;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hey how are you?");
+        Scanner scn = new Scanner(System.in);
+        String v=scn.nextLine();
+        System.out.println("Your reply "+ v);
+
+
+    }
+}
---
+++ src/vanshPackage/Primitives.java
@@ -0,0 +1,14 @@
+package vanshPackage;
+
+public class Primitives {
+    public static void main(String[] args) {
+        //primitive datatypes
+        int rolln = 64;
+        char letter = 'r';
+        float marks = 91.70f;
+        double largeDecimalNum =45678.3723;
+        long largeInteger =34556778989890L;
+        boolean check=true;
+
+    }
+}
