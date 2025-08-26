6c15576374f89073b84456c7a46f7467f326145c
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-08-26 22:50:26 +0530

    multiple if-else

--- src/Conditions_Loops/if_else.java
+++ src/Conditions_Loops/if_else.java
@@ -3,12 +3,22 @@ package Conditions_Loops;
 public class if_else {
     public static void main(String[] args) {
         int salary = 10000;
-        if(salary > 5000){
+//        if(salary > 5000){
+//            salary += 2000;
+//        }
+//        else {
+//            salary += 1000;
+//        }
+//        System.out.println("Your Salary + bonus : "+ salary);
+//
+        //multiple if else condition
+        if(salary>10000){
             salary += 2000;
+        } else if (salary > 20000) {
+            salary+=3000;
+        }else {
+            salary+=4000;
         }
-        else {
-            salary += 1000;
-        }
-        System.out.println("Your Salary + bonus : "+ salary);
+        System.out.println(salary);
     }
 }
