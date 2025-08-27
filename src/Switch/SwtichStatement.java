863d8c230e65700a467e69a75782605fbfc53cda
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-08-27 17:13:02 +0530

    Switch statement

---
+++ src/Switch/SwtichStatement.java
@@ -0,0 +1,26 @@
+package Switch;
+
+import java.util.Scanner;
+
+public class SwtichStatement {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Fruit name:");
+        String fruit = scn.next();
+
+
+        switch (fruit){
+            case "Mango":
+                System.out.println("King of Fruits");
+                break;
+            case "Apple":
+                System.out.println("A sweet red fruit");
+                break;
+            case "Orange":
+                System.out.println("Round fruit");
+                break;
+            default:
+                System.out.println("Please enter a valid fruit");
+        }
+    }
+}
