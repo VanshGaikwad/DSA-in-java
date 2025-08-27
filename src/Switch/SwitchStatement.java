01540f28e41894123d008a8d5a35cda0118b67c7
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-08-27 17:18:53 +0530

    switch statement for day

--- src/Switch/SwitchStatement.java
+++ src/Switch/SwitchStatement.java
@@ -5,22 +5,34 @@ import java.util.Scanner;
 public class SwitchStatement {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.println("Enter Fruit name:");
-        String fruit = scn.next();
+//        System.out.println("Enter Fruit name:");
+//        String fruit = scn.next();
 
 
-        switch (fruit){
-            case "Mango":
-                System.out.println("King of Fruits");
-                break;
-            case "Apple":
-                System.out.println("A sweet red fruit");
-                break;
-            case "Orange":
-                System.out.println("Round fruit");
-                break;
-            default:
-                System.out.println("Please enter a valid fruit");
+//        switch (fruit){
+//            case "Mango":
+//                System.out.println("King of Fruits");
+//                break;
+//            case "Apple":
+//                System.out.println("A sweet red fruit");
+//                break;
+//            case "Orange":
+//                System.out.println("Round fruit");
+//                break;
+//            default:
+//                System.out.println("Please enter a valid fruit");
+//        }
+
+        System.out.println("Enter day value:");
+        int day = scn.nextInt();
+        switch (day) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
         }
     }
 }
