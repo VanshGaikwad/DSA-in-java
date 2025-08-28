7f5a9398bf2227488292dc8f15ad7571e353c6c6
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-08-28 13:26:26 +0530

    return type example

---
+++ src/Functions/StringExample.java
@@ -0,0 +1,12 @@
+package Functions;
+
+public class StringExample {
+    public static void main(String[] args) {
+        System.out.println(greet());
+
+    }
+    static String greet(){
+        String greeting =" how are you guys";
+        return greeting;
+    }
+}
