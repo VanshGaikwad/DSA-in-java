657593a8814c32f40e7f6119b0f382e06093829d
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-08-28 13:23:09 +0530

    greeting

---
+++ src/Functions/Greetings.java
@@ -0,0 +1,10 @@
+package Functions;
+
+public class Greetings {
+    public static void main(String[] args) {
+        greeting();
+    }
+    static  void greeting(){
+        System.out.println("Hello git hub users");
+    }
+}
