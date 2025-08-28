7de101444c9ac6bfb47149b657d73854c5b54a7f
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-08-28 13:54:28 +0530

    oveloading

---
+++ src/Functions/Overloading.java
@@ -0,0 +1,14 @@
+package Functions;
+
+public class Overloading {
+    public static void main(String[] args) {
+    fun(2);
+    fun("vansh");
+    }
+    static  void fun(int a ){
+        System.out.println(a);
+    }
+    static void fun(String name){
+        System.out.println(name);
+    }
+}
