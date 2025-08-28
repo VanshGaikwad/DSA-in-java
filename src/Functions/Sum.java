abf947f0334506cae97e8b007c8c4bacdd0dc384
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-08-28 13:20:35 +0530

    Functions :- sum of two no.

---
+++ src/Functions/Sum.java
@@ -0,0 +1,31 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args)
+    {
+        sum();
+    }
+
+
+    /*
+    access modifier return_type name (){
+    //    body
+    return statement;
+    }
+*/
+
+   static void sum (){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter num 1:");
+        int num1 = in.nextInt();
+        System.out.print("Enter num 2:");
+        int num2 = in.nextInt();
+        int sum = num1 + num2 ;
+        System.out.println("The sum :"+sum);
+    }
+
+}
+
+
---
+++ src/Functions/function.java
@@ -0,0 +1,7 @@
+package Functions;
+//Q: make function for printing sum of 2 numbers
+public class function {
+    public static void main(String[] args) {
+
+    }
+}
