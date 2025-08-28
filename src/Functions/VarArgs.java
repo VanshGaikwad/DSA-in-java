777edd8091cc317698aa13510ec87e9cef5f1265
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-08-28 13:52:23 +0530

    VarArgs

--- src/Functions/VarArgs.java
+++ src/Functions/VarArgs.java
@@ -5,9 +5,14 @@ import java.util.Arrays;
 public class VarArgs {
     public static void main(String[] args) {
         fun(1,2,3,4,5,6,7);
+        multiple(2,3,"Vansh","Elon musk");
     }
 
     static  void fun(int ...v){
         System.out.println(Arrays.toString(v));
+    }
+
+    static void multiple( int a, int b , String ...v){
+        System.out.println(a+" "+b+" "+Arrays.toString(v));
     }
 }
