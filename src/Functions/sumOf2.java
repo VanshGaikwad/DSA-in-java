6ce265fc1d224d5b00a1812ce3c1018d36df8302
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-08-28 13:28:40 +0530

    passing value

---
+++ src/Functions/sumOf2.java
@@ -0,0 +1,13 @@
+package Functions;
+
+public class sumOf2 {
+    public static void main(String[] args) {
+       int a =20;
+       int b=30;
+        System.out.println( sum1(a,b));
+    }
+
+    static int sum1(int a , int b){
+        return a+b;
+    }
+}
