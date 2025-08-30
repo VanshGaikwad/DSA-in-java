14975c8aaeb331ddf7be95143bb278f7c520d518
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-08-30 11:57:36 +0530

    MultiArrayList

---
+++ src/ArrayList/MultiArrayList.java
@@ -0,0 +1,20 @@
+package ArrayList;
+import java.util.*;
+public class MultiArrayList {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+//        Initialization part
+        for(int i = 0;i < 3;i++ ){
+            list.add(new ArrayList<>());
+        }
+//        adding element
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                list.get(i).add(in.nextInt());
+            }
+        }
+        System.out.print(list);
+
+    }
+}
