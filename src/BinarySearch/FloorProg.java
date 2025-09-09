1a001ecc7fa166b5dd8da7f59923e109bd9ad335
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-09-09 12:25:36 +0530

    Floor

---
+++ src/BinarySearch/FloorProg.java
@@ -0,0 +1,22 @@
+package BinarySearch;
+
+public class FloorProg {
+    public static void main(String[] args) {
+        int [] arr = {2,3,5,9,14,16,18};
+        int target= 15;
+        System.out.println("Floor:"+Floor(arr,target));
+
+    }
+    static  int Floor(int [] arr ,int target){
+        int start = 0, end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            if (arr[mid] == target) return arr[mid];  // return value directly
+            else if (target < arr[mid]) end = mid - 1;
+            else start = mid + 1;
+        }
+
+        return (start < arr.length) ? arr[end] : -1;
+    }
+}
